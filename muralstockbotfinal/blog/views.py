ee2c645beae74b6e74255d7b192aec82e1d010c5
joao-dias12 <jpgd94@poli.ufrj.br>
2021-04-01 18:14:51 -0300

    Adionando limite de 10 mensagens
    Colocando método de limitar o numero de mensagens por pagina

--- muralstockbotfinal/blog/views.py
+++ muralstockbotfinal/blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 class PostListView(ListView): # Criei as classes para herdarem as views e depois crei a variavel model" para receberem o valor de Post(classe criada em models.py) , ou seja os posts
     model = Post
+    paginate_by = 10
     
     
 class PostDetailView(DetailView):
