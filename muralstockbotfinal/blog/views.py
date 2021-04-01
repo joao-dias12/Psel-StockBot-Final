c53ef7f175e089b42eafbcf0380bbf7c10017472
joao-dias12 <jpgd94@poli.ufrj.br>
2021-04-01 17:11:01 -0300

    Adionando novos posts
    Adiocionei posts novos para teste de paginação

--- muralstockbotfinal/blog/views.py
+++ muralstockbotfinal/blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 
 class PostListView(ListView): # Criei as classes para herdarem as views e depois crei a variavel model" para receberem o valor de Post(classe criada em models.py) , ou seja os posts
     model = Post
+    
+    
 class PostDetailView(DetailView):
     model = Post
 
